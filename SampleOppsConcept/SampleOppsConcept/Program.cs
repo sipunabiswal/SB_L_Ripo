b617e91fa2c7f52873906b9e79af99a17a4b3f79
sipuna.biswal <Sipuna.biswal@shell.com>
2021-03-11 12:49:05 +0530

    Created new branch for OOPs concept

---
+++ SampleOppsConcept/SampleOppsConcept/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleOppsConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

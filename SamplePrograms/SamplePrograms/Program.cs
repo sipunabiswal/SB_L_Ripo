e63aed1c2dff19641d65c4e52525042a5c79ea4e
Sipuna Biswal <sipuna.biswal@shell.com>
2021-03-20 15:24:08 +0530

    new sample codes

---
+++ SamplePrograms/SamplePrograms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SamplePrograms
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int inputInt = Convert.ToInt32( Console.ReadLine());
+            Double inputDouble = Convert.ToDouble(Console.ReadLine());
+            string inputStr = Console.ReadLine();
+
+            Console.WriteLine(inputInt + inputDouble);
+            Console.WriteLine(inputDouble + inputDouble);
+            Console.WriteLine("S" + inputStr);
+
+                Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}

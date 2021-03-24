2255435320b0a497aba18c60472354524ccade95
Sipuna Biswal <sipuna.biswal@shell.com>
2021-03-24 17:42:50 +0530

    all sample codes added

---
+++ SamplePrograms/SamplePrograms/Narcissistic.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+
+namespace SamplePrograms
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            int input, sum = 0,digitCount=0,num;
+            int.TryParse(Console.ReadLine(), out input);
+
+            for (int i = input; i > 0; i = i / 10)
+            {
+                digitCount++;
+            }
+            num = input;
+            while (num > 0)
+            {
+                sum += (int)Math.Pow(num % 10, digitCount);
+                num /= 10;
+            }
+
+            bool isNarcissistic;
+            if (sum == input)
+            {
+                isNarcissistic = true;
+            }
+            else
+                isNarcissistic = false;
+
+            Console.WriteLine(isNarcissistic);
+        }
+    }
+}
+*/
